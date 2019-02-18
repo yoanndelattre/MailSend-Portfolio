32c3af7be016fc30b94e56f7823432e6c110251d
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-18 18:21:32 +0100

    update

--- index.js
+++ index.js
@@ -7,11 +7,47 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/api/form', (req, res) => {
-    console.log(req.body)
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'nxev5735p4tdrtqe@ethereal.email',
+                pass: 'cvMbA4H1T5JgG7nzAe'
+            }
+        })
+
+        let mailOptions = {
+            from: 'notify@portfolio.com',
+            to: 'yoanndelattre21@gmail.com', 
+            subject: "New message from Portfolio Website",
+            text: req.body.message,
+            html: htmlEmail
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err)
+            }
+
+            console.log('Message sent: %s', info.message)
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+        })
+    })
 })
 
-const PORT = process.env.PORT || 80
+const port = process.env.PORT || 80
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })
