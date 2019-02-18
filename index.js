059ea7995a6af2464686b397414237f4cc77b2bf
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-18 16:08:25 +0100

    add nodemail function

--- index.js
+++ index.js
@@ -7,7 +7,43 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/api/form', (req, res) => {
-    console.log(req.body)
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'nxev5735p4tdrtqe@ethereal.email',
+                pass: 'cvMbA4H1T5JgG7nzAe'
+            }
+        })
+
+        let mailOptions = {
+            from: 'notify@perso-portfolio.com',
+            to: 'contact@yoanndelattre.com',
+            subject: "New message from Portfolio Website",
+            text: req.body.message,
+            html: htmlEmail
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err)
+            }
+
+            console.log('Message sent: %s', info.message)
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+        })
+    })
 })
 
 const port = process.env.PORT || 80
