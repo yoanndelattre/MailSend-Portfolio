66ddfe1b6577274d1ae7a212838c719b7449d24b
Yoann Delattre <yoanndelattre21@hmail.com>
2019-03-17 10:50:29 +0100

    remove next

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
 })
 
 app.post('/api/form', (req, res) => {
