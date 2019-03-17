eb5a717c3b38fb84232426d220820448172c7f35
Yoann Delattre <yoanndelattre21@hmail.com>
2019-03-17 10:48:49 +0100

    update header app

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err, account) => {
         const htmlEmail = `
