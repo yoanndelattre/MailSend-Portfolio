320ebc35063b2bb247cf0675f73af0ef42dd3072
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-31 19:11:22 +0200

    update

--- index.js
+++ index.js
@@ -1,65 +1,60 @@
-const nodemailer = require('nodemailer');
-const express = require('express');
-var cors = require('cors')
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
 
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 
-}
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
-app.post('/mail/send', function (req, res, next) {
-    
-    let mailOpts, smtpTrans;
-    
-    smtpTrans = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-          user: 'portfolio.yoanndelattre@gmail.com',
-          pass: 'f3Nk50n8f8qF',
-      },
-    });
+app.post('/mail/send', (req, res) => {
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact: </h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <br></br>
+            <h3>Message: </h3>
+            <p>${req.body.message}</p>
+        `
 
-    const htmlEmail = `
-        <h3>Contact: </h3>
-        <ul>
-            <li>Name: ${req.body.name}</li>
-            <li>Email: ${req.body.email}</li>
-        </ul>
-        <br></br>
-        <h3>Message: </h3>
-        <p>${req.body.message}</p>
-    `
-    
-    mailOpts = {
-        from: 'notify@portfolio.com',
-        to: 'contact@yoanndelattre.com,yoanndelattre.bsmhxq@zapiermail.com',
-        replyTo: `${req.body.email}`,
-        subject: "New message from Portfolio Website",
-        text: req.body.message,
-        html: htmlEmail,
-    };
-    
-    smtpTrans.sendMail(mailOpts, function (error, response) {
-      
-      if (error) {
-        console.log(error)
-      }
-      
-      else {
-        console.log('success')
-      }
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'portfolio.yoanndelattre@gmail.com',
+                pass: 'f3Nk50n8f8qF'
+            }
+        })
 
-    });
+        let mailOptions = {
+            from: 'notify@portfolio.com',
+            to: 'contact@yoanndelattre.com,yoanndelattre.bsmhxq@zapiermail.com',
+            replyTo: `${req.body.email}`,
+            subject: "New message from Portfolio Website",
+            text: req.body.message,
+            html: htmlEmail,
+        }
 
-});
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err)
+            }
 
-// Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+            console.log('Success mail send')
+        })
+    })
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
