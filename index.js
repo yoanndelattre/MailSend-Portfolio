65886a302e313feb3bc2ac1b70a87643e2d6c435
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-31 17:07:48 +0200

    update

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-
-app.post('/mail/send', function (req, res) {
+app.post('/mail/send', function (req, res, next) {
     
     let mailOpts, smtpTrans;
     
