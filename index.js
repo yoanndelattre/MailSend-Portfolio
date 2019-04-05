f9cec25878681f10c4fa003893bfad4dd29fa617
Yoann Delattre <yoanndelattre21@gmail.com>
2019-04-05 22:16:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.post('/mail/send', (req, res) => {
             port: 465,
             secure: true,
             auth: {
-                user: 'portfolio.yoanndelattre@gmail.com',
-                pass: 'niEHo&QJIqj5%ZA2RDaVSr7JjCZ'
+                user: process.env.GMAIL_USER ,
+                pass: process.env.GMAIL_PASS
             }
         })
 
