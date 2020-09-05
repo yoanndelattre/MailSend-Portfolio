ec25f1b33acf31261b4b0a482720d0ef818ec2c9
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-05 17:29:36 +0200

    Test: set post request

--- test/test.js
+++ test/test.js
@@ -1,23 +1,30 @@
 const request = require('supertest');
-describe('--Test MailSend App--', function() {
-  let server;
+describe('--Test MailSend App-- \n', function() {
   beforeEach(function() {
     server = require('../index.js');
   });
   afterEach(function() {
     server.close();
   });
-  it('responds to / (302)', function testSlash(done) {
+  it('POST /mail/send', function(done) {
+    request(server)
+        .post('/mail/send')
+        .set('Accept', 'application/json')
+        .send({test: 'supertest_test'})
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .expect(200, done);
+  });
+  it('GET /', function(done) {
     request(server)
         .get('/')
         .expect(302, done);
   });
-  it('responds to /robots.txt (200)', function testPathRobots(done) {
+  it('GET /robots.txt', function(done) {
     request(server)
         .get('/robots.txt')
         .expect(200, done);
   });
-  it('responds to /mail/send (404)', function testPathRobots(done) {
+  it('GET /mail/send', function(done) {
     request(server)
         .get('/mail/send')
         .expect(404, done);
