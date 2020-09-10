d01d0b30bb7d682a85ab8c2153ea4f6713df0743
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-10 20:28:01 +0200

    CORS: add cors function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const MailAdmin = require('./assets/MailAdmin/MailAdmin');
 const MailClientFR = require('./assets/MailClient/MailClientFR');
 const MailCLientUS = require('./assets/MailClient/MailClientUS');
 
-app.use(cors({
-  origin: 'https://yoanndelattre.com',
-  credentials: true,
-}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://yoanndelattre.com');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
