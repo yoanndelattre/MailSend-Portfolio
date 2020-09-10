c1abd0210fea21e2ca2fa39de7b42a6043a085f3
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-10 14:54:11 +0200

    CORS: change ACAH rules

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://yoanndelattre.com');
   res.header('Access-Control-Allow-Methods', 'POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
